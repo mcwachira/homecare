f88cbc4cc2a586f478a520434f49fe755a0c061f
mcwachira <mcwachira@outlook.com>
2018-11-22 09:05:45 +0300

    made first changes

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@ function getScrollTop(){
         //most browsers except IE before #9
         return pageYOffset;
     } else {
-        var B = document.body; //IE 'quirks'
-        var D = document.documentElement; //IE with doctype
+        let B = document.body; //IE 'quirks'
+        let D = document.documentElement; //IE with doctype
         D = (D.clientHeight) ? D : B;
         return D.scrollTop;
     }
 }
 
-var timeouts = [];
-var scrolling = false;
-var scroller;
-var scrollTop = getScrollTop();
-var timeMs;
-var alter = false;
-var speed = 5;
+let timeouts = [];
+let scrolling = false;
+let scroller;
+let scrollTop = getScrollTop();
+let timeMs;
+let alter = false;
+let speed = 5;
 window.onscroll = function() {
     if(alter) {
-        var timeDif = new Date().getMilliseconds() - timeMs;
+        let timeDif = new Date().getMilliseconds() - timeMs;
         speed = 5 - (timeDif / 50);
         console.log(speed);
     }
     timeMs = new Date().getMilliseconds();
     alter = !alter;
-    var scrollDirection = getScrollTop() - scrollTop;
+    let scrollDirection = getScrollTop() - scrollTop;
     scrollDirection = scrollDirection / Math.abs(scrollDirection);
     scrollTop = getScrollTop();
     clearTimeout(scroller);
@@ -34,14 +34,14 @@ window.onscroll = function() {
         if(!scrolling) {
             timeouts.length = 0;
             scrolling = true;
-            var steps = 50;
-            var delay = 6;
-            for(var i = 0; i < steps; i++) {
+            let steps = 50;
+            let delay = 6;
+            for(let i = 0; i < steps; i++) {
                 (function(i){
-                    var timeout = setTimeout(function(){
-                        var perc = i / steps;
-                        var val = (perc == 1) ? 1 : (-Math.pow(2, -10 * perc) + 1);
-                        var scrollY = val * speed * scrollDirection;
+                    let timeout = setTimeout(function(){
+                        let perc = i / steps;
+                        let val = (perc == 1) ? 1 : (-Math.pow(2, -10 * perc) + 1);
+                        let scrollY = val * speed * scrollDirection;
                         window.scrollTo(0, getScrollTop() + scrollY);
                         setTimeout(function(){
                             if(i == (steps - 1)) scrolling = false;
@@ -53,3 +53,28 @@ window.onscroll = function() {
         }
     }, 50);
 };
+
+/* affix the navbar after scroll below header */
+// $('nav').affix({
+//       offset: {
+//         top: $('#services').offset().top
+//       }
+// });
+
+
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        slideSpeed:1000,
+        autoPlay:true
+    });
+});
+
+
